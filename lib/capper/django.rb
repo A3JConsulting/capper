95e37e736cda454bfa6f65b71361747f518cf8c5
Joel Bonander <joel.bonander@a3j.se>
2013-11-03 23:51:53 +0100

    settings fixes to django tasks

--- lib/capper/django.rb
+++ lib/capper/django.rb
@@ -2,9 +2,13 @@ load "capper/python"
 
 after 'deploy:update_code', 'django:setup'
 
+before 'django:setup', 'django:apply_settings'
+
 before 'deploy:migrate', 'django:migrate'
 
 after "deploy:finalize_update", "django:collectstatic"
+
+_cset(:django_settings) { "#{application}.settings" }
 
 namespace :django do
   desc "Generate django configuration and helpers"
@@ -35,9 +39,33 @@ namespace :django do
   end
 
   desc <<-DESC
-    Run the collectstatic task
+    Run the collectstatic task.
   DESC
   task :collectstatic, :roles => :app, :except => { :no_release => true } do
+    target = fetch(:target, :latest)
+
+    directory = case target.to_sym
+      when :current then current_path
+      when :latest  then latest_release
+      else raise ArgumentError, "unknown target #{target.inspect}"
+      end
+
     run "cd #{directory} && #{python} manage.py collectstatic --noinput"
+  end
+
+  desc <<-DESC
+    Apply the current stage's settings file.
+  DESC
+  task :apply_settings, :except => { :no_release => true } do
+    target = fetch(:target, :latest)
+
+    directory = case target.to_sym
+      when :current then current_path
+      when :latest  then latest_release
+      else raise ArgumentError, "unknown target #{target.inspect}"
+      end
+
+    run("if [ -e #{directory}/#{application}/settings.#{stage}.py ]; then " +
+        "cp #{directory}/#{application}/settings.#{stage}.py #{directory}/#{application}/settings.py; fi")
   end
 end
