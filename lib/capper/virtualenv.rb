acc758feb2eff8d199be530d5377ab2751009a43
Joel Bonander <joel.bonander@a3j.se>
2013-11-03 23:52:25 +0100

    Fixed broken task pip:install

--- lib/capper/virtualenv.rb
+++ lib/capper/virtualenv.rb
@@ -9,7 +9,7 @@ before "deploy:setup", "virtualenv:setup"
 after "deploy:finalize_update", "pip:install"
 
 namespace :virtualenv do
-  desc "Create virtualenv for Python packages"
+  desc "Create virtualenv for Python packages."
   task :setup, :except => {:no_release => true} do
     run("if [ ! -e #{bin_path}/python ]; then " +
         "virtualenv -q --no-site-packages #{deploy_to}; fi")
@@ -17,7 +17,20 @@ namespace :virtualenv do
 end
 
 namespace :pip do
+  desc "List installed python packages."
+  task :list_packages do
+    run("#{bin_path}/pip freeze")
+  end
+
+  desc "Install required python packages."
   task :install do
-    run("#{bin_path}/pip install -q -E #{deploy_to} -r #{latest_release}/#{python_requirements_file}")
+    from_release = fetch(:from_release, :latest)
+
+    directory = case from_release.to_sym
+      when :current then current_path
+      when :latest  then latest_release
+      else raise ArgumentError, "unknown release #{target.inspect}"
+      end
+    run "cd #{directory} && #{bin_path}/pip install -q -r #{python_requirements_file}"
   end
 end
