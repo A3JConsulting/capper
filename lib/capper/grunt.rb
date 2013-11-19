805d53aa7a04b6607f5ac64e7878a11a4bef6e92
Joel Bonander <joel.bonander@a3j.se>
2013-11-19 19:08:51 +0100

    Finalized grunt sub module

--- lib/capper/grunt.rb
+++ lib/capper/grunt.rb
@@ -1,6 +1,7 @@
-after 'deploy:update_code', 'grunt:setup'
+#after 'deploy:update_code', 'grunt:setup'
 
-#after "deploy:finalize_update", 'grunt:run_tasks'
+after "deploy:finalize_update", 'grunt:run_tasks'
+before "grunt:run_tasks", "grunt:setup"
 
 _cset(:grunt_tasks, "dist")
 _cset(:grunt_path, "assets")
@@ -10,29 +11,13 @@ _cset(:npm_bin, "npm") # Should maybe be #{bin_path}/npm
 namespace :grunt do
   desc "Generate django configuration and helpers"
   task :setup, :roles => :app, :except => { :no_release => true } do
-    target = fetch(:target, :latest)
-
-    directory = case target.to_sym
-      when :current then current_path
-      when :latest  then latest_release
-      else raise ArgumentError, "unknown target #{target.inspect}"
-      end
-
-    print :directory
-
-    #run "cd #{directory} && #{npm_bin} install"
+    grunt_abs_path = File.join(current_release, grunt_path)
+    run "cd #{grunt_abs_path} && #{npm_bin} install"
   end
 
-  #desc "Run grunt tasks"
-  #task :run_tasks, :roles => :app, :except => { :no_release => true } do
-  #  target = fetch(:target, :latest)
-
-  #  directory = case target.to_sym
-  #    when :current then current_path
-  #    when :latest  then latest_release
-  #    else raise ArgumentError, "unknown target #{target.inspect}"
-  #    end
-
-  #  run "cd #{directory} && #{grunt_bin} #{grunt_tasks}"
-  #end
+  desc "Run grunt tasks"
+  task :run_tasks, :roles => :app, :except => { :no_release => true } do
+    grunt_abs_path = File.join(current_release, grunt_path)
+    run "cd #{grunt_abs_path} && #{grunt_bin} #{grunt_tasks}"
+  end
 end
